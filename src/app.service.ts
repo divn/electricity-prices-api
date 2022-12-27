ca23287178aad4b62ddb89101238044db34d9aa2
Juuso Takala <juuso.takala86@gmail.com>
2022-12-28 01:11:12 +0200

    elering, todayandtomorrow working for finland

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
+// @ts-nocheck
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { map } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {}
+
+  getTodayAndTomorrow(_params): string {
+    let start = new Date(new Date().setHours(23, 59, 59, 999));
+    start.setDate(start.getDate() - 1);
+    start = start.toISOString();
+
+    let end = new Date(new Date().setHours(23, 59, 59, 999));
+    end.setDate(end.getDate() + 1);
+    end = end.toISOString();
+
+    return this.httpService
+      .get(
+        `https://dashboard.elering.ee/api/nps/price?start=${start}&end=${end}`,
+      )
+      .pipe(
+        map((response) => response.data),
+        map((data) => data.data.fi),
+      );
   }
 }
